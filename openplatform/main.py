847af42b2c158b8af6db6e5f15058308b3908b07
eldiiar.egemberdiev <eldiiar.egemberdiev@zensoft.io>
2018-07-20 18:32:45 +0600

    simplified validation

--- openplatform/main.py
+++ openplatform/main.py
@@ -4,7 +4,7 @@ from openplatform.senders import Scaffold, Shareholder
 class OpenPlatform(object):
     def __init__(self, open_key=''):
         self.headers = {'Authorization': open_key}
-        if self.headers['Authorization'] == '':
+        if open_key == '':
             raise AttributeError('open_key can not be empty')
         self.scaffold = Scaffold(self.headers)
         self.shareholder = Shareholder(self.headers)
