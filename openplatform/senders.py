cf1310cbbf41195862f7954ea8c266b14388eec1
eldiiar.egemberdiev <eldiiar.egemberdiev@zensoft.io>
2018-07-27 12:07:59 +0600

    added a message of the response in exceptions

--- openplatform/senders.py
+++ openplatform/senders.py
@@ -10,48 +10,48 @@ class Scaffold:
 
     def get_all(self):
         res = requests.get(base('scaffolds'), headers=self.headers)
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def get_single(self, address):
         validate_address(address)
         res = requests.get(base('scaffolds/' + address), headers=self.headers)
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def get_summary(self, address):
         validate_address(address)
         res = requests.get(base('scaffolds/' + address + '/summary'), headers=self.headers)
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def get_transactions(self, address):
         validate_address(address)
         res = requests.get(base('scaffolds/' + address + '/transactions'), headers=self.headers)
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def get_quota(self):
         res = requests.get(base('scaffolds/quota'), headers=self.headers)
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def deploy(self, data):
         res = requests.post(base('scaffolds/doDeploy'), json=data, headers=merge_headers([CONTENT_JSON, self.headers]))
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def deactivate(self, address):
         validate_address(address)
         res = requests.delete(base('scaffolds/' + address), headers=merge_headers([CONTENT_JSON, self.headers]))
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def set_webhook(self, address, data):
         validate_address(address)
         res = requests.patch(base('scaffolds/' + address), json=data,
                              headers=merge_headers([CONTENT_JSON, self.headers]))
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
 
@@ -63,19 +63,29 @@ class Shareholder:
         validate_address(address)
         res = requests.post(base('scaffolds/' + address + '/holders'), json=data,
                             headers=merge_headers([CONTENT_JSON, self.headers]))
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def update(self, address, holder_address, data):
         validate_address(address)
         res = requests.put(base('scaffolds/' + address + '/holders/' + holder_address), json=data,
                            headers=merge_headers([CONTENT_JSON, self.headers]))
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
 
     def remove(self, address, holder_address):
         validate_address(address)
         res = requests.delete(base('scaffolds/' + address + '/holders/' + holder_address),
                               headers=merge_headers([CONTENT_JSON, self.headers]))
-        res.raise_for_status()
+        raise_for_error(res)
         return res.json()
+
+
+def raise_for_error(res):
+    try:
+        res.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        if res.text:
+            raise requests.exceptions.HTTPError(res.json().get('message'))
+        else:
+            raise e
