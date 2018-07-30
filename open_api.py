230bdf37b5373b59f1b5a9c5c93ee17d6bca423d
eldiiar.egemberdiev <eldiiar.egemberdiev@zensoft.io>
2018-07-30 13:00:43 +0600

    moved open_key check upper

--- open_api.py
+++ open_api.py
@@ -3,8 +3,8 @@ from openplatform.senders import Scaffold, Shareholder
 
 class OpenPlatform(object):
     def __init__(self, open_key=''):
-        self.headers = {'Authorization': open_key}
         if open_key == '':
             raise AttributeError('open_key can not be empty')
+        self.headers = {'Authorization': open_key}
         self.scaffold = Scaffold(self.headers)
         self.shareholder = Shareholder(self.headers)
