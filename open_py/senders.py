b464a673f8bedf143c47c4b7d67c794921e72cc0
Aibek Abykeev <aibek.abykeev@zensoft.io>
2019-12-11 10:41:26 +0000

    urls were updated

--- open_py/senders.py
+++ open_py/senders.py
@@ -4,78 +4,78 @@ from open_py.urls import base
 from open_py.utils import validate_address, merge_headers, CONTENT_JSON
 
 
-class Scaffold:
+class EthereumScaffold:
     def __init__(self, headers=None):
         self.headers = headers
 
     def get_all(self):
-        res = requests.get(base('scaffolds'), headers=self.headers)
+        res = requests.get(base('ethereum-scaffolds'), headers=self.headers)
         raise_for_error(res)
         return res.json()
 
     def get_single(self, address):
         validate_address(address)
-        res = requests.get(base('scaffolds/' + address), headers=self.headers)
+        res = requests.get(base('ethereum-scaffolds/' + address), headers=self.headers)
         raise_for_error(res)
         return res.json()
 
     def get_summary(self, address):
         validate_address(address)
-        res = requests.get(base('scaffolds/' + address + '/summary'), headers=self.headers)
+        res = requests.get(base('ethereum-scaffolds/' + address + '/summary'), headers=self.headers)
         raise_for_error(res)
         return res.json()
 
     def get_transactions(self, address):
         validate_address(address)
-        res = requests.get(base('scaffolds/' + address + '/transactions'), headers=self.headers)
+        res = requests.get(base('ethereum-scaffolds/' + address + '/transactions'), headers=self.headers)
         raise_for_error(res)
         return res.json()
 
     def get_quota(self):
-        res = requests.get(base('scaffolds/quota'), headers=self.headers)
+        res = requests.get(base('ethereum-scaffolds/quota'), headers=self.headers)
         raise_for_error(res)
         return res.json()
 
     def deploy(self, data):
-        res = requests.post(base('scaffolds/doDeploy'), json=data, headers=merge_headers([CONTENT_JSON, self.headers]))
+        res = requests.post(base('ethereum-scaffolds/doDeploy'), json=data, headers=merge_headers([CONTENT_JSON, self.headers]))
         raise_for_error(res)
         return res.json()
 
     def deactivate(self, address):
         validate_address(address)
-        res = requests.delete(base('scaffolds/' + address), headers=merge_headers([CONTENT_JSON, self.headers]))
+        res = requests.delete(base('ethereum-scaffolds/' + address), headers=merge_headers([CONTENT_JSON, self.headers]))
         raise_for_error(res)
         return res.json()
 
     def set_webhook(self, address, data):
         validate_address(address)
-        res = requests.patch(base('scaffolds/' + address), json=data,
+        res = requests.patch(base('ethereum-scaffolds/' + address), json=data,
                              headers=merge_headers([CONTENT_JSON, self.headers]))
         raise_for_error(res)
         return res.json()
 
 
-class Shareholder:
+class EthereumShareholder:
     def __init__(self, headers=None):
         self.headers = headers
 
     def create(self, address, data):
         validate_address(address)
-        res = requests.post(base('scaffolds/' + address + '/holders'), json=data,
+        res = requests.post(base('ethereum-scaffolds/' + address + '/holders'), json=data,
                             headers=merge_headers([CONTENT_JSON, self.headers]))
         raise_for_error(res)
         return res.json()
 
     def update(self, address, holder_address, data):
         validate_address(address)
-        res = requests.put(base('scaffolds/' + address + '/holders/' + holder_address), json=data,
+        res = requests.put(base('ethereum-scaffolds/' + address + '/holders/' + holder_address), json=data,
                            headers=merge_headers([CONTENT_JSON, self.headers]))
         raise_for_error(res)
         return res.json()
 
     def remove(self, address, holder_address):
         validate_address(address)
-        res = requests.delete(base('scaffolds/' + address + '/holders/' + holder_address),
+        res = requests.delete(base('ethereum-scaffolds/' + address + '/holders/' + holder_address),
                               headers=merge_headers([CONTENT_JSON, self.headers]))
         raise_for_error(res)
         return res.json()
